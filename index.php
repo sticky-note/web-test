6c54f1346eda9aff32b6a3d60b10512f6c9b46e2
sticky-note <46799934+sticky-note@users.noreply.github.com>
2019-02-28 16:42:27 +1100

    test mysqli

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php phpinfo(); ?>
+<?php
+  $mysqli = new mysqli( "localhost", "app_dba", "app_pwd", "app_db" );
+  /* check connection */
+  if ( $mysqli->connect_errno ) {
+    printf( "Connect failed: %s\n", $mysqli->connect_error );
+    exit();
+  }
+
+  $ctable = $mysqli->query( "CREATE TABLE IF NOT EXISTS test ( id int, test varchar(10) );" );
+  $insert = $mysqli->query( "INSERT INTO test VALUES ( 1, 'OOOOOOOOOO');" );
+  $query = "SELECT * FROM test;";
+
+  if ( $result = $mysqli->query( $query ) ) {
+    /* fetch associative array */
+    printf( "<html>\n<head></head>\n<body>" );
+    while ( $row = $result->fetch_assoc() ) {
+      printf( "<div>%s - (%s)</div>", $row["id"], $row["test"] );
+    }
+    /* free result set */
+    $result->free();
+    printf( "</body>\n</html>" )
+  }
+
+  /* close connection */
+  $mysqli->close();
+?>
